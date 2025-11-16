684d4df3781a85b45e502c5ab3524610e399598b
xavtry <sebastianishot68@gmail.com>
2025-11-16 11:37:22 -0500

    Update tabManager.js

--- frontend/ src/utils/tabManager.js
+++ frontend/ src/utils/tabManager.js
@@ -1,1 +1,24 @@
 
+export function createTab(url = "home") {
+  return {
+    id: Date.now(),
+    title: url,
+    url,
+    active: true,
+  };
+}
+
+export function activateTab(tabs, id) {
+  return tabs.map((t) => ({ ...t, active: t.id === id }));
+}
+
+export function closeTab(tabs, id) {
+  let newTabs = tabs.filter((t) => t.id !== id);
+
+  // if no active tab, activate last one
+  if (!newTabs.find((t) => t.active) && newTabs.length > 0) {
+    newTabs[newTabs.length - 1].active = true;
+  }
+
+  return newTabs;
+}
