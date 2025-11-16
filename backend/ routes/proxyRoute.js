a89f62ef95aefbca9a6e04a9e58ca585b4a01293
xavtry <sebastianishot68@gmail.com>
2025-11-16 11:39:29 -0500

    Update proxyRoute.js

--- backend/ routes/proxyRoute.js
+++ backend/ routes/proxyRoute.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const fetchProxy = require("../utils/fetchProxy");
+
+router.get("/", async (req, res) => {
+  const url = req.query.url;
+  const result = await fetchProxy(url);
+  if (!result) return res.send("Proxy error.");
+  res.send(result);
+});
+
+module.exports = router;
 
