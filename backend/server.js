dad9aeb1948118216e3c40ee33e59cb9c1c261c3
xavtry <sebastianishot68@gmail.com>
2025-11-16 11:34:59 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
 
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(cors());
+
+app.get("/proxy", async (req, res) => {
+  const target = req.query.url;
+
+  if (!target) return res.send("No URL provided.");
+
+  try {
+    const result = await axios.get(target, {
+      responseType: "arraybuffer"
+    });
+
+    res.set("Content-Type", result.headers["content-type"]);
+    res.send(result.data);
+
+  } catch (err) {
+    res.send("Proxy Error: " + err.message);
+  }
+});
+
+app.listen(4000, () => console.log("SUB v3 Proxy Running on :4000"));
