478361278ddec208f9954bebc562f744906e6533
xavtry <sebastianishot68@gmail.com>
2025-11-16 11:32:18 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,13 @@
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/proxy': 'http://localhost:4000'
+    }
+  }
+});
