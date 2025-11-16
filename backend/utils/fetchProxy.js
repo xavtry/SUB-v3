db39e92c5fc36c723e3885d23b4ccce6e0fcc221
xavtry <sebastianishot68@gmail.com>
2025-11-16 11:39:45 -0500

    Update fetchProxy.js

--- backend/utils/fetchProxy.js
+++ backend/utils/fetchProxy.js
@@ -1,1 +1,13 @@
+const axios = require("axios");
+
+module.exports = async function fetchProxy(url) {
+  try {
+    const res = await axios.get(url, {
+      responseType: "arraybuffer"
+    });
+    return res.data;
+  } catch (err) {
+    return null;
+  }
+};
 
